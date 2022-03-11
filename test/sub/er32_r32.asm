94fb6b21b5f1327ff46dd1bea592be1c56dd1e86
Patrick Flynn <patrickf4664@protonmail.com>
2022-03-11 08:39:16 -0500

    Added sub r32 tests (all variants, no imm, mem)

---
+++ test/sub/er32_r32.asm
@@ -0,0 +1,17 @@
+.intel_syntax noprefix
+.data
+
+.text
+.global _start
+_start:
+    mov edi, 15
+    mov r10d, 10
+    sub r10d, edi
+    mov edi, r10d
+    
+    mov eax, 60
+    syscall
+done:
+    ret
+    
+    
